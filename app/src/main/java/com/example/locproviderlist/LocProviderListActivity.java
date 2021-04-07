32003b60636d5c2e4a7436377145785eb5a453c3
JJW <ckjjw2953@naver.com>
2021-04-07 15:47:38 +0900

    enhanced for loop

--- app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Range;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,10 +31,11 @@ public class LocProviderListActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String s = "";
-                for (int i = 0; i < locProviders.size(); i++){
-                    s += "Loc. Provider " + locProviders.get(i) + "\n"
-                            + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+                for (String i : locProviders){
+                    s += "Loc. Provider " + i + "\n"
+                            + "Status: " + locMgr.isProviderEnabled(i) + "\n\n";
                 }
                 mTextView.setText(s);
             }
